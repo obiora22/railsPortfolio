fb52f145df92ce6f832cd12cd3ac983033c056f0
obi <obianaedozie@gmail.com>
2018-02-22 18:26:18 -0500

    Add Faker gem and seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |i| 
+  title = "Blog #{i + 1}"
+  body = Faker::Lorem.paragraph
+  Blog.create!({title: title, body: body})
+end 
+
+Skill.create!({title: "Rails", proficiency_percent: 50}) 
+Skill.create!({title: "JavaScript", proficiency_percent: 70}) 
+Skill.create!({title: "CSS", proficiency_percent: 60}) 
+Skill.create!({title: "ReactJS", proficiency_percent: 50}) 
+Skill.create!({title: "HTML", proficiency_percent: 60}) 
+
+9.times do |i|
+  Portfolio.create!(
+    title: "Portfolio #{i}",
+    subtitle: "Subtitle #{i}",
+    body: Faker::Lorem.paragraph,
+    main_image: "main_image#{i}",
+    thumb_image: "thumb_image#{i}"
+  )
+end 
